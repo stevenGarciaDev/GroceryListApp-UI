a501945522a9aa8f499eee5743cfdc43caec6bc8
Steven Garcia <stevgarcia2015@gmail.com>
2021-02-16 16:05:27 -0800

    Implement sign up component

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth/auth.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  styleUrls: [
+    '../login/login.component.css',
+    './sign-up.component.css'
+  ]
 })
 export class SignUpComponent implements OnInit {
+  isDisplayingFormErrors = false;
+  signUpForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.minLength(5), Validators.required]),
+  });
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  signUp(): void {
+    this.isDisplayingFormErrors = true;
+    this.authService.registerUser(this.signUpForm.value)
+      .subscribe(response => console.log('response', response));
+  }
+
+  isValidForm(): string {
+    return '';
+  }
+
+  get name(): any {
+    return this.signUpForm.get('name');
+  }
+
+  get email(): any {
+    return this.signUpForm.get('email');
+  }
+
+  get password(): any {
+    return this.signUpForm.get('password');
   }
 
 }
