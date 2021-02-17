f907925f9b0b8a66c1a04e824b25e312f1173c73
Steven Garcia <stevgarcia2015@gmail.com>
2021-02-16 16:14:42 -0800

    Update list component

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GroceryListService } from '../../services/grocery-list/grocery-list.service';
 
 @Component({
   selector: 'app-list',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class ListComponent implements OnInit {
   userName = 'Steven';
 
-  constructor() { }
+  constructor(private listService: GroceryListService) { }
 
   ngOnInit(): void {
   }
